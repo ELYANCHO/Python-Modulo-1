2b4aaae92a6e5ac43ee3cb7533237fec4328dc58
Elyan Alejandro Cristancho Castañeda <elyancristancho123@gmail.com>
2025-08-01 16:04:53 -0500

    Ejercicio 4

---
+++ Ejercicio 4.py
@@ -0,0 +1,14 @@
+def tabla_de_multiplicar(numero):
+  """
+  Genera la tabla de multiplicar para un número dado.
+
+  Args:
+    numero: El número del cual se generará la tabla.
+  """
+  for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+
+numero_usuario = int(input("Ingrese un número para generar la tabla de multiplicar: "))
+
+tabla_de_multiplicar(numero_usuario)
